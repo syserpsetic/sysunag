0b78ac8edcc0c2904c170d91c770f19e909d5ef9
syserpsetic <syserpsetic@unag.edu.hn>
2025-04-03 14:34:26 -0600

    login_20250403_1434

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -8,37 +8,37 @@
       <div class="card">
         <div class="row">
           <div class="col-md-4 pe-md-0">
-            <div class="auth-side-wrapper" style="background-image: url({{ url('https://via.placeholder.com/219x452') }})">
+            <div class="auth-side-wrapper" style="background-image: url({{ url(asset('/assets/images/favicon.ico')) }})">
 
             </div>
           </div>
           <div class="col-md-8 ps-md-0">
             <div class="auth-form-wrapper px-4 py-5">
-              <a href="#" class="noble-ui-logo d-block mb-2">Noble<span>UI</span></a>
-              <h5 class="text-muted fw-normal mb-4">Welcome back! Log in to your account.</h5>
+              <a href="#" class="noble-ui-logo d-block mb-2">SYS <span>UNAG</span></a>
+              <h5 class="text-muted fw-normal mb-4">¡BIENVENIDOS! INICIA SESIÓN PARA INGRESAR</h5>
               <form class="forms-sample">
                 <div class="mb-3">
-                  <label for="userEmail" class="form-label">Email address</label>
-                  <input type="email" class="form-control" id="userEmail" placeholder="Email">
+                  <label for="userEmail" class="form-label">Usuario o Correo Electrónico</label>
+                  <input type="email" class="form-control" id="userEmail" placeholder="Escribe tu usuario o correo electrónico">
                 </div>
                 <div class="mb-3">
-                  <label for="userPassword" class="form-label">Password</label>
-                  <input type="password" class="form-control" id="userPassword" autocomplete="current-password" placeholder="Password">
+                  <label for="userPassword" class="form-label">Contraseña</label>
+                  <input type="password" class="form-control" id="userPassword" autocomplete="current-password" placeholder="Escribe tu contraseña">
                 </div>
-                <div class="form-check mb-3">
+                <!-- <div class="form-check mb-3">
                   <input type="checkbox" class="form-check-input" id="authCheck">
                   <label class="form-check-label" for="authCheck">
                     Remember me
                   </label>
-                </div>
+                </div> -->
                 <div>
-                  <a href="{{ url('/') }}" class="btn btn-primary me-2 mb-2 mb-md-0">Login</a>
-                  <button type="button" class="btn btn-outline-primary btn-icon-text mb-2 mb-md-0">
+                  <a href="{{ url('/') }}" class="btn btn-primary me-2 mb-2 mb-md-0">Ingresar</a>
+                  <!-- <button type="button" class="btn btn-outline-primary btn-icon-text mb-2 mb-md-0">
                     <i class="btn-icon-prepend" data-feather="twitter"></i>
                     Login with twitter
-                  </button>
+                  </button> -->
                 </div>
-                <a href="{{ url('/auth/register') }}" class="d-block mt-3 text-muted">Not a user? Sign up</a>
+                <!-- <a href="{{ url('/auth/register') }}" class="d-block mt-3 text-muted">Not a user? Sign up</a> -->
               </form>
             </div>
           </div>
