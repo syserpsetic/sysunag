ca9cb11d70b25b17d7a50f910cf8014af8d89401
syserpsetic <syserpsetic@unag.edu.hn>
2025-05-15 09:28:43 -0600

    20250515_0928

--- app/Http/Controllers/MallaValidacion/MallaValidacionController.php
+++ app/Http/Controllers/MallaValidacion/MallaValidacionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\MallaValidacion;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\View\View;
 use DB;
 Use Session;
