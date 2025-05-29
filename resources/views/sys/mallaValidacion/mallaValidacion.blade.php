4bfda3fd163bf16f90b2b5b116911d30d4716ce4
syserpsetic <syserpsetic@unag.edu.hn>
2025-05-29 11:54:31 -0600

    20250529_1154

--- resources/views/sys/mallaValidacion/mallaValidacion.blade.php
+++ resources/views/sys/mallaValidacion/mallaValidacion.blade.php
@@ -706,7 +706,7 @@
                 clase_indicador_matricula = 'feather feather-chevrons-down icon me-2 text-danger';
                 polyline_indicador_matricula = '<polyline points="7 13 12 18 17 13"></polyline><polyline points="7 6 12 11 17 6"></polyline>';
             }
-            $("#indicador_matricula").html('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="'+clase_indicador_carga_academica+'">'+polyline_indicador_matricula+'</svg>');
+            $("#indicador_matricula").html('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="'+clase_indicador_matricula+'">'+polyline_indicador_matricula+'</svg>');
         }
 
             const indicadores = [
