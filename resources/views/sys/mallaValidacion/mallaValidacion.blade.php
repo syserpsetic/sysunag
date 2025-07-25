14835e22ffb8721b0c2841691ec8d9bc2d61202a
syserpsetic <syserpsetic@unag.edu.hn>
2025-07-25 13:22:00 -0600

    20250725_1321

--- resources/views/sys/mallaValidacion/mallaValidacion.blade.php
+++ resources/views/sys/mallaValidacion/mallaValidacion.blade.php
@@ -1004,6 +1004,34 @@
             }, 30000); // Verifica cada 30 segundos
         })();
 
+        (function () {
+            const claveStorage = 'llamado_' + new Date().toISOString().slice(0, 10); // Solo una vez por día
+
+            // Evitar repetir la reproducción
+            if (localStorage.getItem(claveStorage)) return;
+
+            // Verificar la hora cada 30 segundos
+            const intervalo = setInterval(() => {
+                const ahora = new Date();
+                const hora = ahora.getHours();
+                const minutos = ahora.getMinutes();
+
+                // Condición: 4:30 PM o después
+                if ((hora > 13 || (hora === 13 && minutos >= 00)) && !localStorage.getItem(claveStorage)) {
+                    const mensaje = "Se hace un llamado a todos los compañeros y practicantes: el tiempo de descanso ha concluido. Por favor, retomemos nuestras labores con el mismo compromiso y responsabilidad. ¡Gracias!";
+
+                    responsiveVoice.speak(mensaje, "Spanish Latin American Female", {
+                        rate: 1.1,
+                        pitch: 1,
+                        volume: 1
+                    });
+
+                    localStorage.setItem(claveStorage, '1');
+                    clearInterval(intervalo); // Detener verificación después de reproducir
+                }
+            }, 30000); // Verifica cada 30 segundos
+        })();
+
         const newsContainer = document.getElementById("newsContainer");
             let position = window.innerWidth;
 
